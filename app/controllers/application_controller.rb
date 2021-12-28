c5dd5f91d5b33f140f4ab7cf9d9bf36dfbe6f3e5
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2021-12-28 08:04:57 +0000

    userの検索機能はできた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :set_search, except: [:top, :about]
 
-  private
-  def set_search
-    @search = Search.new(search_params)
-  end
 
 
 
--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,37 +1,42 @@
 class SearchsController < ApplicationController
 
   def search
-    @model = params["model"]
-    @content = params["content"]
-    @method = params["method"]
-    @records = search_for(@model, @content, @method)
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+    @results = search_for(@model, @content, @method)
+    if @model = "User"
+      @relationship = current_user.relationships.find_by(follow_id: @results.ids)
+      @new_relationship = current_user.relationships.new
+
+    end
   end
 
   private
   def search_params
-    params.require().permit(:model, :content, :metho)
+    params.require(:search).permit(:model, :content, :method)
   end
-  
-  
+
+
   def search_for(model, content, method)
-    if @model == 'user'
-      if @method == 'perfect'
+    if @model == "User"
+      if @method == "perfect"
         User.where(name: content)
-      elsif @method == 'partial'
+      elsif @method == "partial"
         User.where('name LIKE ?', '%'+content+'%')
-      elsif @method == 'forward'
+      elsif @method == "forward"
         User.where('name LIKE ?', content+'%')
-      elsif @method == 'backward'
+      elsif @method == "backward"
         User.where('name LIKE ?', '%'+content)
       end
-    elsif @model == 'book'
-      if @method == 'perfect'
+    elsif @model == "Book"
+      if @method == "perfect"
         Book.where(title: content)
-      elsif @method == 'partial'
+      elsif @method == "partial"
         Book.where('title LIKE ?', '%'+content+'%')
-      elsif @method == 'forward'
+      elsif @method == "forward"
         Book.where('title LIKE ?', content+'%')
-      elsif @method == 'backward'
+      elsif @method == "backward"
         Book.where('title LIKE ?', '%'+content)
       end
     end
