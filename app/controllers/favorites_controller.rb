7ff022c4ab5a5f53c1c0c69d11e97c6e800c364a
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2022-01-04 08:18:04 +0000

    ファボの非同期完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,15 @@
 class FavoritesController < ApplicationController
 
-  before_action :set_book
-
-  def set_book
-    @book = Book.find(params[:book_id])
-  end
-
 
   def create
+    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
