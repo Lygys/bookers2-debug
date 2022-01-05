378d3fcd75f9a8b830c8d348fd57396e8f364d43
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2022-01-05 11:15:42 +0000

    7a完成2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,13 @@
 class Book < ApplicationRecord
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
+	has_many :favorited_users, through: :favorites, source: :user
 	has_many :book_comments, dependent: :destroy
 
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
 
   def self.sort_by_favorites
 	  select('books.*', 'count(favorites.id) AS favs')
