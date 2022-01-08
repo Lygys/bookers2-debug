1718b84fcded3bc881467b61f4e2b1abb3c60355
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2022-01-08 02:56:09 +0000

    作業中断。7a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_040849) do
+ActiveRecord::Schema.define(version: 2022_01_08_024934) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -39,6 +39,24 @@ ActiveRecord::Schema.define(version: 2022_01_06_040849) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.string "image_id"
+    t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
