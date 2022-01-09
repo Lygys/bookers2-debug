2743fba79b5a08a647d66fd00347b68150a2918f
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2022-01-09 10:26:57 +0000

    作業中断

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,10 +13,15 @@ class BooksController < ApplicationController
   def index
     to  = Time.current.at_end_of_day
     from  = (to - 6.day).at_beginning_of_day
-    @books = Book.all.sort {|a,b|
-            b.favorites.where(created_at: from...to).size <=>
-            a.favorites.where(created_at: from...to).size
-          }
+    @sort_list = Book.sort_list
+    if sort_params.present?
+      @books = Book.sort_books(sort_params)
+    else
+      @books = Book.all.sort {|a,b|
+                b.favorites.where(created_at: from...to).size <=>
+                a.favorites.where(created_at: from...to).size
+              }
+    end
   end
 
   def create
@@ -60,6 +65,10 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body, :evaluation)
+  end
+
+  def sort_params
+    params.permit(:sort)
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,15 @@ class Book < ApplicationRecord
 	has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
 	has_many :book_comments, dependent: :destroy
 
+	scope :sort_books, -> (sort) { order(sort[:sort]) }
+  scope :sort_list, -> {
+    {
+      "並び替え" => "",
+      "評価の高い順" => "evaluation DESC",
+      "新着順" => "created_at DESC"
+    }
+  }
+
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
